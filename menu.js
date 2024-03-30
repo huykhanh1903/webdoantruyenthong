b64c6976a898cbe628049ac93735bef9c440e5c0
unknown <phantrongvt1234@gmail.com>
2024-03-30 18:36:31 +0700

    menu hoan thien

--- menu.js
+++ menu.js
@@ -23,11 +23,47 @@ function Timmonan()
         document.getElementById("Bun").style.display="none";
         document.getElementById("Banhmy").style.display="block";
     }
+    else
+    {
+        document.getElementById("Pho").style.display="block";
+        document.getElementById("Bun").style.display="block";
+        document.getElementById("Banhmy").style.display="block";
+    }
+    var x = document.getElementById("chongiaTien").value;
+    var tamgia1= document.getElementsByClassName("tamgia1");
+    var tamgia2= document.getElementsByClassName("tamgia2");
+    var d = document.getElementsByClassName("Themonan")
+    if(x=="tamgia1")
+    {
+    for(let i =0;i<tamgia2.length;i++)
+    {
+        tamgia2[i].style.display = "none";
+    }
+    for(let i =0;i<tamgia1.length;i++)
+    {
+        tamgia1[i].style.display = "block";
+    }
+    
+
+    }
+    if(x=="tamgia2")
+    {
+    for(let i =0;i<tamgia1.length;i++)
+    {
+        tamgia1[i].style.display = "none";
+    }
+    for(let i =0;i<tamgia2.length;i++)
+    {
+        tamgia2[i].style.display = "block";
+    }
+    }
 }
 }
+
 //Them tim cho mon an trong menu
 function doiso()
 {
     var s = parseInt(document.getElementById("daylaso").value);
-    document.getElementById("daylaso").innerHTML=s;
+    document.getElementById("daylaso").innerHTML="1201";
 }
+//kiem tra menu theo gia tien
