31d6846b0c3977acd11e359cfeea48cbe997670d
unknown <phantrongvt1234@gmail.com>
2024-04-02 12:09:04 +0700

    animation don gia

---
+++ baitapcuoikyweb.js
@@ -0,0 +1,10 @@
+function Chaynguocanh()
+{
+    document.getElementById("gioithieu").style.animation = "doianh 4s infinite reverse";
+    
+}
+function Chaynguocanh2()
+{
+    document.getElementById("gioithieu").style.animation=" doianh 4s infinite alternate"
+    
+}
